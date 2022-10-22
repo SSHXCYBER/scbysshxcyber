ac9a0bd91ee97083b95c042a3fc885388dc02372
SSHXCYBER <114498865+SSHXCYBER@users.noreply.github.com>
2022-10-22 19:25:21 +0700

    Update and rename scv1.0 to scv1.0.py

---
+++ scv1.0.py
@@ -0,0 +1,79 @@
+
+================================
+      Script From SSHXCYBER
+================================
+      
+     
+   Hello world how are you?, hope you are fine.On this occasion I will share the scripts for the TERMUX application that you may need, therefore I am sharing the script now, and you just have to take it for free.
+   But you also need to know that I got this script not from our own organization, but from several sites or applications.If you want to use it please copy it.
+
+   Alright for those who want to use it I will give the script below : 
+
+1. Script Termux Shell
+Beautify your Termux App .
+Installation & Usage
+$ apt update
+$ apt install git -y
+$ git clone https://github.com/htr-tech/termux-shell.git
+$ cd termux-shell
+$ chmod +x *
+$ sh install.sh
+$ exit
+
+2. Script Termux Ubnutu
+termux-ubuntu = A script to install Ubuntu chroot in Termux
+
+Installation :
+
+$ apt update && apt upgrade
+$ apt install git 
+$ apt install proot
+$ apt install wget
+$ git clone https://github.com/Neo-Oli/termux-ubuntu
+$ cd termux-ubuntu
+$ ls
+$ chmod +x *
+$ sh ubuntu.sh
+
+Run :
+
+After running it you can run "start-ubuntu.sh" to switch into your ubuntu
+
+$ ./start.sh
+Now you are in Ubuntu terminal..
+
+*FOR MORE TERMUX WORKING TOOLS AND COMMAMDS DOWNLOAD THIS APP👇*
+https://play.google.com/store/apps/details?id=termux.working.tools.commands
+
+3. Script Termux Wafwoof
+WAFW00f is the inbuilt tool in Kali distribution or
+
+else you can install it manually.
+
+we can install this tool in termux also
+
+It can detect around Top 22 web application firewall,
+
+so wafw00f is a phase of information gathering initially.
+
+follow these steps :
+
+Installation :
+$ apt update && apt upgrade
+$ apt install python2
+$ apt install python
+$ pip install wafw00f
+
+usage : 
+$ wafw00f -h
+
+It shows all options of wafw00f tool how you can use this tool
+$ wafw00f [url]
+
+*FOR MORE TERMUX WORKING TOOLS AND COMMAMDS DOWNLOAD THIS APP👇*
+https://play.google.com/store/apps/details?id=termux.working.tools.commands
+
+That's the first script that I can convey at this time, please comment if something is missing and maybe next time I will add it, THANK YOU
+
+    
+
